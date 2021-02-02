11981f315b9e8cf599c9ae492b37b23830330807
rddlamas@gmail.com <rlama005@hammer.cs.ucr.edu>
2021-02-01 22:41:32 -0800

    Fixed/enhanced the personList.cpp destructor

--- personList.cpp
+++ personList.cpp
@@ -12,7 +12,10 @@ PersonList::PersonList(){
 }
 
 PersonList::~PersonList(){
-    delete [] theList;
+for(int i = 0; i< numPeople; ++i) {
+	delete theList[i];
+}
+   delete [] theList;
 }
 
 void PersonList::addPerson(const char* child_name, const char* father_name, const char* mother_name){
@@ -48,8 +51,8 @@ void PersonList::addPerson(const char* child_name, const char* father_name, cons
     father->addChild(newChild);
     mother->addChild(newChild);
 
-//delete[] father;
-//delete[] mother;
+//delete father;
+//delete mother;
 //delete newChild;
 //father = NULL;
 //mother = NULL;
